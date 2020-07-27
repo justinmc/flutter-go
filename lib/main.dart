a6e053f54388585a4827626d517e4966f5454495
Justin McCandless <jmccandless@google.com>
2020-07-27 15:05:15 -0700

    Draggable pieces

--- lib/main.dart
+++ lib/main.dart
@@ -42,12 +42,89 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: InteractiveViewer(
-          child: Image.asset(
-          'images/go_board.png',
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: InteractiveViewer(
+              child: Image.asset(
+              'images/go_board.png',
+              ),
+            ),
           ),
-        ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              height: 120.0,
+              color: Colors.grey,
+              child: Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    _DraggablePiece(
+                      team: _Team.black,
+                    ),
+                    _DraggablePiece(
+                      team: _Team.white,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+enum _Team {
+  black,
+  white,
+}
+
+class _Piece extends StatelessWidget {
+  _Piece({
+    Key key,
+    this.team,
+    this.isDragging = false,
+  }) : assert(team != null),
+       super(key: key);
+
+  final bool isDragging;
+  final _Team team;
+
+  @override
+  Widget build(BuildContext context) {
+    final double opacity = isDragging ? 0.4 : 1.0;
+    return Container(
+      color: team == _Team.black ? Colors.black.withOpacity(opacity) : Colors.white.withOpacity(opacity),
+      width: 40.0,
+      height: 40.0,
+    );
+  }
+}
+
+class _DraggablePiece extends StatelessWidget {
+  _DraggablePiece({
+    Key key,
+    this.team,
+    this.isDragging = false,
+  }) : assert(team != null),
+       super(key: key);
+
+  final bool isDragging;
+  final _Team team;
+
+  @override
+  Widget build(BuildContext context) {
+    return Draggable(
+      feedback: _Piece(
+        isDragging: true,
+        team: team,
+      ),
+      child: _Piece(
+        team: team,
       ),
     );
   }
