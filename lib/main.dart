2e967aa550ab4be34f68e66fdffafca2457a38b7
Justin McCandless <jmccandless@google.com>
2020-07-27 15:50:10 -0700

    Can drop on board, but when transformed it's not right

--- lib/main.dart
+++ lib/main.dart
@@ -13,43 +13,26 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Go'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Stack(
         children: <Widget>[
           Center(
-            child: InteractiveViewer(
-              child: Image.asset(
-              'images/go_board.png',
-              ),
-            ),
+            child: _Board(),
           ),
           Align(
             alignment: Alignment.centerLeft,
@@ -81,6 +64,67 @@ class _MyHomePageState extends State<MyHomePage> {
 enum _Team {
   black,
   white,
+}
+
+class _PieceData {
+  const _PieceData({
+    this.offset,
+    this.team,
+  });
+
+  final Offset offset;
+  final _Team team;
+}
+
+class _Board extends StatefulWidget {
+  _Board({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _BoardState createState() => _BoardState();
+}
+
+class _BoardState extends State<_Board> {
+  final GlobalKey _dragTargetKey = GlobalKey();
+  final List<_PieceData> _pieces = <_PieceData>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return InteractiveViewer(
+      child: DragTarget<_Team>(
+        key: _dragTargetKey,
+        onAcceptWithDetails: (DragTargetDetails details) {
+          final RenderBox renderBox = _dragTargetKey.currentContext.findRenderObject();
+          final Offset dragTargetOffset = renderBox.localToGlobal(Offset.zero);
+          final Offset offset = details.offset - dragTargetOffset;
+          setState(() {
+            _pieces.add(_PieceData(
+              offset: offset,
+              team: details.data,
+            ));
+          });
+        },
+        onWillAccept: (_Team team) {
+          return true;
+        },
+        builder: (BuildContext context, List<_Team> candidateData, List rejectedData) {
+          return Stack(
+            children: <Widget>[
+              Image.asset(
+                'images/go_board.png',
+              ),
+              ..._pieces.map((_PieceData pieceData) => Positioned(
+                top: pieceData.offset.dy,
+                left: pieceData.offset.dx,
+                child: _Piece(team: pieceData.team),
+              )).toList(),
+            ],
+          );
+        },
+      ),
+    );
+  }
 }
 
 class _Piece extends StatelessWidget {
@@ -118,7 +162,8 @@ class _DraggablePiece extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Draggable(
+    return Draggable<_Team>(
+      data: team,
       feedback: _Piece(
         isDragging: true,
         team: team,
